c83a2dd82d7cafb488da0769924dd329590624d6
Chris <corez92@gmail.com>
2018-06-02 16:06:26 +0200

    lets fuck shit up....issue #3 trying to get tests to work

--- mysite/stock_data/tests.py
+++ mysite/stock_data/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+class DataFetchingTests(TestCase):
+
+    def is_there_data_in_the_variable(self):
+        """
+        Is OHLC_data populated with data?
+        """
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def did_the_user_enter_a_valid_symbol(self):
+    	
--- mysite/stock_data/views.py
+++ mysite/stock_data/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
 
 from bs4 import BeautifulSoup
@@ -16,11 +16,11 @@ def index(request):
 def result(request):
 	stock_name = "Facebook"
 	stock_symbol = request.POST['stock_symbol'].upper()
-
+ 
 	parameter_options = {"TIME_SERIES_INTRADAY": 
-							{"function": "TIME_SERIES_INTRADAY", "symbol": stock_symbol.upper(), "interval": "1min", "apikey": api_key},
+							{"function": "TIME_SERIES_INTRADAY", "symbol": stock_symbol, "interval": "1min", "apikey": api_key},
 						"TIME_SERIES_DAILY": 
-							{"function": "TIME_SERIES_DAILY", "symbol": stock_symbol.upper(), "apikey": api_key}}
+							{"function": "TIME_SERIES_DAILY", "symbol": stock_symbol, "apikey": api_key}}
 	parameters = parameter_options["TIME_SERIES_DAILY"]
 	response = requests.get('https://www.alphavantage.co/query?', params= parameters)
 
@@ -28,7 +28,6 @@ def result(request):
 	last_update_time = json_data["Meta Data"]["3. Last Refreshed"]
 	last_update_data = json_data["Time Series (Daily)"][last_update_time]
 	ohlc_data = last_update_data
-	#total_volume
 
 	markets_open_date = "2018-06-01" # 09:30:00"
 	timestamp_data = "09:30:00"

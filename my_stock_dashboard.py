55737cb45ed9aa687e694c70589049290e17a798
Chris <corez92@gmail.com>
2018-06-02 11:03:24 +0200

    django time

--- my_stock_dashboard.py
+++ my_stock_dashboard.py
@@ -1,10 +1,7 @@
-# import libraries
-#import urllib2
 from bs4 import BeautifulSoup
 import requests
 import json
 
-# https://www.alphavantage.co/ API Key
 api_key = "MQ6XW7KDB2DF9M5O"
 
 try:
@@ -12,24 +9,17 @@ try:
 except:
     stock = raw_input("What stock are you interested in? \n")
 
- 
-
 parameters = {"function": "TIME_SERIES_INTRADAY", "symbol": stock.upper(), "interval": "1min", "apikey": api_key}
 response = requests.get('https://www.alphavantage.co/query?', params= parameters)
 
 json_data = json.loads(response.content)
 last_update_time = json_data["Meta Data"]["3. Last Refreshed"]
 last_update_data = json_data["Time Series (1min)"][last_update_time]
+total_volume
 
-#data = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=' + stock + '&interval=1min&apikey=' + api_key
+markets_open_date = "2018-06-01" # 09:30:00"
+timestamp_data = "09:30:00"
 
-#print(data)
-
-#<span class="IsqQVc NprOob i4ncfvccpph4-zJFzKq8ukm8">1,607.97</span>
-
-##response = requests.get(g_data)
-##soup = BeautifulSoup(response.content, 'html.parser')
-##posts = soup.find_all('tr', class_='athing comtr ')
 
 
 
